1edd7d45bfbd586599706c238b85f8a0ae0e9134
Allen Dai <allendai1@hotmail.com>
2024-04-11 16:59:36 -0400

    .

---
+++ import-map.js
@@ -0,0 +1,12 @@
+// import-map.js
+
+console.log('x')
+const importMap = {
+    imports: {
+      "chart": "./node_modules/chart.js/auto/auto.d.ts",
+
+    }
+  };
+  
+  export default importMap;
+  
